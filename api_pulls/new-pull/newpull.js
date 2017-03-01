e692185a480caf368818ac2dfe2d0cf163ed5d7b
Christina Bergstrom <cbergstrom@avzdo.com>
2017-03-01 16:42:43 -0500

    more code

--- api_pulls/new-pull/newpull.js
+++ api_pulls/new-pull/newpull.js
@@ -2,12 +2,13 @@ $(document).ready(function(){
   $.get("https://private-8e550-directmail.apiary-mock.com/templates/categories", function(goodness){
     var apiaryArray = goodness.categories;
     for(var i=0; i<apiaryArray.length; i++){
-      var thumbnail = apiaryArray[i].name;
-      var title = apiaryArray[i].subcategories.name;
+      var templateCategory = apiaryArray[i].name;
+      var subCategory = apiaryArray[i].subcategories.name;
       $('section').append(
-        '<div><h2>' + thumbnail +
-        '</h2>'+ title +
+        '<div><h2>' + templateCategory +
+        '</h2>'+ subCategory +
         '</div>')
     }
+    console.log(apiaryArray);
   });
 });
