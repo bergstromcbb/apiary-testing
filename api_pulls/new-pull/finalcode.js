d2c47d09b7618df04164717981ce181d5147d9ff
Christina Bergstrom <bergstromcbb@gmail.com>
2017-03-02 08:14:13 -0500

    newest working code

---
+++ api_pulls/new-pull/finalcode.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+  $.get("https://private-8e550-directmail.apiary-mock.com/templates/categories", function(goodness) {
+    var apiaryArray = goodness.categories;
+    for(var i = 0; i < apiaryArray.length; i++){
+      var templateCategory = apiaryArray[i].name;
+      $('section').append('<div class=section' + i + '><h2>' + templateCategory + '</h2></div>');
+      for(var j = 0; j < apiaryArray[i].subcategories.length; j++){
+        var subCategory = apiaryArray[i].subcategories[j].name;
+        $('.section' + i).append('<p>' + subCategory + '</p>');
+
+      }
+
+    }
+
+  });
+
+});
--- api_pulls/new-pull/newpull.js
+++ api_pulls/new-pull/newpull.js
@@ -1,14 +1,51 @@
+// $(document).ready(function(){
+//
+//
+//
+//   $.get("https://private-8e550-directmail.apiary-mock.com/templates/categories").success(function(goodness){
+//     var apiaryArray = goodness.categories;
+//     for(var i=0; i<apiaryArray.length; i++){
+//       var templateCategory = apiaryArray[i].name;
+//
+//       var subp =  $("<p></p>");
+//
+//       for(var j=0; j<apiaryArray[i].subcategories.length; j++){
+//         var subCategory = apiaryArray[i].subcategories[j].name;
+//         console.log(apiaryArray[i].subcategories[j].name);
+//
+//         subp.text(subCategory);
+//
+//       }
+//
+//
+//       $('section').append(
+//         '<div><h2>' + templateCategory +
+//         '</h2>'+  subp +
+//         '</div>');
+//
+//
+//     }
+//     console.log(apiaryArray);
+//
+//   });
+// });
+
 $(document).ready(function(){
+
   $.get("https://private-8e550-directmail.apiary-mock.com/templates/categories", function(goodness){
     var apiaryArray = goodness.categories;
+
     for(var i=0; i<apiaryArray.length; i++){
       var templateCategory = apiaryArray[i].name;
-      var subCategory = apiaryArray[i].subcategories.name;
-      $('section').append(
-        '<div><h2>' + templateCategory +
-        '</h2>'+ subCategory +
-        '</div>')
+
+      $('section').append('<div><h2>' + templateCategory + '</h2></div>');
+
+      for(var j=0; j<apiaryArray[i].subcategories.length; j++){
+        var subCategory = apiaryArray[i].subcategories[j].name;
+        console.log(apiaryArray[i].subcategories[j].name);
+        $('section div').append('<p>' + subCategory + '</p>');
+      }
     }
-    console.log(apiaryArray);
   });
+
 });
