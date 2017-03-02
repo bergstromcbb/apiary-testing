a3ca99cbe2d1c4519d9f0fe434557c5b599bda2a
Christina Bergstrom <cbergstrom@avzdo.com>
2017-03-02 14:02:16 -0500

    final stuff

--- api_pulls/new-pull/finalcode.js
+++ api_pulls/new-pull/finalcode.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
     var apiaryArray = goodness.categories;
     for(var i = 0; i < apiaryArray.length; i++){
       var templateCategory = apiaryArray[i].name;
-      $('section').append('<div class=section' + i + '><h2>' + templateCategory + '</h2></div>');
+      var templateImage = apiaryArray[i].image;
+      $('section').append('<div class=section' + i + '><h2>' + templateCategory + '</h2><img class="picture" src="' + templateImage + '"/></div>');
       for(var j = 0; j < apiaryArray[i].subcategories.length; j++){
         var subCategory = apiaryArray[i].subcategories[j].name;
         $('.section' + i).append('<p>' + subCategory + '</p>');
